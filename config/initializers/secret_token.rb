0d619434d67cc0273f01b8ed2ea1aefd76884acc
Casey Li <casey.li@gmail.com>
2012-08-14 12:54:40 -0400

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Claridgeplaza2::Application.config.secret_token = '7dbd6a99519d65c2a7655a3312d66b6c4f4b6ed32b8d0fafb63b1fdc17867594a88d565f15aeabfc049d16c71b1b061a7c113e81609810f7e3835f19560f44fa'
